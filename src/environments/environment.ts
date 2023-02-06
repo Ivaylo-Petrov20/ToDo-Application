e6609ca0c10468a5d060e8fc6b7a4a697269051b
Ivaylo Petrov <ivodpetrov@gmail.com>
2023-02-06 22:32:15 +0200

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCAxVxRvkiZdZN_tY9pfePh9uPbNtP1CC0",
+    authDomain: "todo-app-981c8.firebaseapp.com",
+    projectId: "todo-app-981c8",
+    storageBucket: "todo-app-981c8.appspot.com",
+    messagingSenderId: "666987562833",
+    appId: "1:666987562833:web:35daa4ba17720cef8d7570"
+  }
+};
